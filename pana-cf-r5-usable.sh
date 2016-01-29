40c93ddef100bf2624ace23126abf58990804bd9
Greg Minshall <minshall@acm.org>
2016-01-29 06:59:51 +0200

    bare bones parsing

--- pana-cf-r5-usable.sh
+++ pana-cf-r5-usable.sh
@@ -13,31 +13,36 @@
 # getopt processing.  see
 # /usr/share/doc/util-linux/examples/getopt-parse.bash
 
-TEMP=`getopt -o ab:c:: --long a-long,b-long:,c-long:: \
-     -n 'example.bash' -- "$@"`
+name=$0
 
-if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+usage() { echo "usage: $name [--cleanup] [--force] [--dir dirname]" >&2; exit 1; }
+
+TEMP=`getopt -n "$name" -o "" --long cleanup,force,dir: -- "$@"`
+
+if [ $? != 0 ] ; then usage; fi # usage() does NOT return
 
 # Note the quotes around `$TEMP': they are essential!
 eval set -- "$TEMP"
 
 while true ; do
-	case "$1" in
-		-a|--a-long) echo "Option a" ; shift ;;
-		-b|--b-long) echo "Option b, argument \`$2'" ; shift 2 ;;
-		-c|--c-long) 
-			# c has an optional argument. As we are in quoted mode,
-			# an empty parameter will be generated if its optional
-			# argument is not found.
-			case "$2" in
-				"") echo "Option c, no argument"; shift 2 ;;
-				*)  echo "Option c, argument \`$2'" ; shift 2 ;;
-			esac ;;
-		--) shift ; break ;;
-		*) echo "Internal error!" ; exit 1 ;;
-	esac
+    case "$1" in
+	--cleanup) echo cleanup; shift ;;
+	--force) echo force; shift ;;
+	--dir) echo dir $2; shift 2 ;;
+	-a|--a-long) echo "Option a" ; shift ;;
+	-b|--b-long) echo "Option b, argument \`$2'" ; shift 2 ;;
+	-c|--c-long) 
+	    # c has an optional argument. As we are in quoted mode,
+	    # an empty parameter will be generated if its optional
+	    # argument is not found.
+	    case "$2" in
+		"") echo "Option c, no argument"; shift 2 ;;
+		*)  echo "Option c, argument \`$2'" ; shift 2 ;;
+	    esac ;;
+	--) shift ; break ;;
+	*) echo "Internal error!" ; exit 1 ;;
+    esac
 done
-echo "Remaining arguments:"
-for arg do echo '--> '"\`$arg'" ; done
+if [ $# != 0 ]; then echo extraneous arguments on command line >&2; usage; fi
 
 

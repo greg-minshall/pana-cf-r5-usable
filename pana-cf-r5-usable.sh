913394c73ba3b2de8ef2bf9be90584fae2feab44
Greg Minshall <minshall@acm.org>
2016-01-29 07:06:10 +0200

    parsing seems (?) to work

--- pana-cf-r5-usable.sh
+++ pana-cf-r5-usable.sh
@@ -24,25 +24,21 @@ if [ $? != 0 ] ; then usage; fi # usage() does NOT return
 # Note the quotes around `$TEMP': they are essential!
 eval set -- "$TEMP"
 
+cleanup=0
+force=0
+dir=/var/tmp/${USER}/${name}
+
 while true ; do
     case "$1" in
-	--cleanup) echo cleanup; shift ;;
-	--force) echo force; shift ;;
-	--dir) echo dir $2; shift 2 ;;
-	-a|--a-long) echo "Option a" ; shift ;;
-	-b|--b-long) echo "Option b, argument \`$2'" ; shift 2 ;;
-	-c|--c-long) 
-	    # c has an optional argument. As we are in quoted mode,
-	    # an empty parameter will be generated if its optional
-	    # argument is not found.
-	    case "$2" in
-		"") echo "Option c, no argument"; shift 2 ;;
-		*)  echo "Option c, argument \`$2'" ; shift 2 ;;
-	    esac ;;
+	--cleanup) cleanup=1; shift ;;
+	--force) force=1; shift ;;
+	--dir) dir=$2; shift 2 ;;
 	--) shift ; break ;;
 	*) echo "Internal error!" ; exit 1 ;;
     esac
 done
+echo cleanup $cleanup, force $force, dir $dir
+
 if [ $# != 0 ]; then echo extraneous arguments on command line >&2; usage; fi
 
 
